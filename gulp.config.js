c7b2adc1a7381cc87ffa0534e5bc33f2dd42a232
Paul Knopf <theonlylawislove@gmail.com>
2015-10-11 22:07:24 -0400

    Added gulp

---
+++ gulp.config.js
@@ -0,0 +1,7 @@
+module.exports = function() {
+
+  var config = {
+  }
+
+  return config;
+};
---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')({ lazy: true });
+
+var config = require('./gulp.config')();
+
+gulp.task('default', function() {
+
+});
